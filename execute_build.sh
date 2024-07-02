1d62a2b917efbdfc33e1a800f83a67667176b6c4
Mariano Fresno <mnofresno@gmail.com>
2024-07-02 01:36:09 -0300

    Add some scripts

--- execute_build.sh
+++ execute_build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 # Copy assets from src to app/assets
-docker-compose exec android-build bash -c /app/scripts/copy_assets.sh
+./copy_assets.sh
 
-docker-compose exec android-build bash -c ./build.sh
+cd /app
+./gradlew assembleDebug
+
 # Check if ADB server is running and restart if necessary
 adb kill-server
 adb start-server
