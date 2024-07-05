791ce00e5c79c139f2e8e66f4ce94fb9f47c1c97
Mariano Fresno <mariano.fresno@avature.net>
2024-07-04 23:07:00 -0300

    Rename directory to android

--- generate_icons.sh
+++ generate_icons.sh
@@ -4,11 +4,11 @@
 SOURCE_IMAGE="icon_for_webview.webp"
 
 # Destination directories
-DEST_DIRS=("app/src/main/res/mipmap-mdpi"
-           "app/src/main/res/mipmap-hdpi"
-           "app/src/main/res/mipmap-xhdpi"
-           "app/src/main/res/mipmap-xxhdpi"
-           "app/src/main/res/mipmap-xxxhdpi")
+DEST_DIRS=("android/src/main/res/mipmap-mdpi"
+           "android/src/main/res/mipmap-hdpi"
+           "android/src/main/res/mipmap-xhdpi"
+           "android/src/main/res/mipmap-xxhdpi"
+           "android/src/main/res/mipmap-xxxhdpi")
 
 # Create directories if they don't exist
 for dir in "${DEST_DIRS[@]}"; do
@@ -16,19 +16,19 @@ for dir in "${DEST_DIRS[@]}"; do
 done
 
 # Resize and copy the images
-convert $SOURCE_IMAGE -resize 48x48 app/src/main/res/mipmap-mdpi/ic_launcher.png
-convert $SOURCE_IMAGE -resize 48x48 app/src/main/res/mipmap-mdpi/ic_launcher_round.png
+convert $SOURCE_IMAGE -resize 48x48 android/src/main/res/mipmap-mdpi/ic_launcher.png
+convert $SOURCE_IMAGE -resize 48x48 android/src/main/res/mipmap-mdpi/ic_launcher_round.png
 
-convert $SOURCE_IMAGE -resize 72x72 app/src/main/res/mipmap-hdpi/ic_launcher.png
-convert $SOURCE_IMAGE -resize 72x72 app/src/main/res/mipmap-hdpi/ic_launcher_round.png
+convert $SOURCE_IMAGE -resize 72x72 android/src/main/res/mipmap-hdpi/ic_launcher.png
+convert $SOURCE_IMAGE -resize 72x72 android/src/main/res/mipmap-hdpi/ic_launcher_round.png
 
-convert $SOURCE_IMAGE -resize 96x96 app/src/main/res/mipmap-xhdpi/ic_launcher.png
-convert $SOURCE_IMAGE -resize 96x96 app/src/main/res/mipmap-xhdpi/ic_launcher_round.png
+convert $SOURCE_IMAGE -resize 96x96 android/src/main/res/mipmap-xhdpi/ic_launcher.png
+convert $SOURCE_IMAGE -resize 96x96 android/src/main/res/mipmap-xhdpi/ic_launcher_round.png
 
-convert $SOURCE_IMAGE -resize 144x144 app/src/main/res/mipmap-xxhdpi/ic_launcher.png
-convert $SOURCE_IMAGE -resize 144x144 app/src/main/res/mipmap-xxhdpi/ic_launcher_round.png
+convert $SOURCE_IMAGE -resize 144x144 android/src/main/res/mipmap-xxhdpi/ic_launcher.png
+convert $SOURCE_IMAGE -resize 144x144 android/src/main/res/mipmap-xxhdpi/ic_launcher_round.png
 
-convert $SOURCE_IMAGE -resize 192x192 app/src/main/res/mipmap-xxxhdpi/ic_launcher.png
-convert $SOURCE_IMAGE -resize 192x192 app/src/main/res/mipmap-xxxhdpi/ic_launcher_round.png
+convert $SOURCE_IMAGE -resize 192x192 android/src/main/res/mipmap-xxxhdpi/ic_launcher.png
+convert $SOURCE_IMAGE -resize 192x192 android/src/main/res/mipmap-xxxhdpi/ic_launcher_round.png
 
 echo "Icons generated and copied to respective directories."
