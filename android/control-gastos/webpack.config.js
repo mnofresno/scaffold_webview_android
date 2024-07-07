111e84010bb36528680adcd4471cd97ebf6f5174
Mariano Fresno <mnofresno@gmail.com>
2024-07-07 13:25:17 -0300

    Fix some fonts and styles

--- android/control-gastos/webpack.config.js
+++ android/control-gastos/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = (env, argv) => {
     const isProduction = argv.mode === 'production';
@@ -16,11 +17,18 @@ module.exports = (env, argv) => {
             rules: [
                 {
                     test: /\.css$/i,
-                    use: ['style-loader', 'css-loader'],
+                    use: [
+                        isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+                        'css-loader'
+                    ],
                 },
                 {
                     test: /\.scss$/i,
-                    use: ['style-loader', 'css-loader', 'sass-loader'],
+                    use: [
+                        isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+                        'css-loader',
+                        'sass-loader'
+                    ],
                 },
                 {
                     test: /\.(png|jpg|gif)$/i,
@@ -36,6 +44,10 @@ module.exports = (env, argv) => {
                 template: './src/index.html',
                 filename: 'index.html',
                 inject: 'body'
+            }),
+            new MiniCssExtractPlugin({
+                filename: '[name].css',
+                chunkFilename: '[id].css'
             })
         ],
         devServer: {
